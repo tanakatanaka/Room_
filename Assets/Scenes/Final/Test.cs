e1d25c203b07058150de640fbdb959a25a00187a
tanakatanaka <aiuro2003haru@gmail.com>
2021-08-14 19:46:32 +0900

    fist commit

---
+++ Assets/Scenes/Final/Test.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    [SerializeField]
+    private Cinemachine.CinemachineSmoothPath path;
+
+    void Update()
+    {
+        transform.position = path.EvaluatePositionAtUnit(0.5f, Cinemachine.CinemachinePathBase.PositionUnits.Normalized);
+    }
+}
