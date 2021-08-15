7358c33f6c5c7db72af91619765be1bea56fc177
tanakatanaka <aiuro2003haru@gmail.com>
2021-08-15 18:32:48 +0900

    モデル追加

---
+++ Assets/Script/CinemachineDollyCartController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CinemachineDollyCartController : MonoBehaviour
+{
+    [SerializeField] CinemachineDollyCart _cinemachineDollyCart;
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            _cinemachineDollyCart.m_Speed = 2;
+            return;
+        }
+
+        _cinemachineDollyCart.m_Speed = 0;
+    }
+
+
+    void SpeedUp()
+    {
+        _cinemachineDollyCart.m_Speed = 2;
+    }
+
+
+}
